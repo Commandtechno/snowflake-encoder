6903736cdee380c31c3143ff903a63575b6cc72b
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2021-10-22 12:49:49 -0500

    fix: encoding and decoding

--- index.ts
+++ index.ts
@@ -6,5 +6,5 @@ export function decode(id: string) {
   return id
     .split("")
     .reduce((total, current) => total * 36n + BigInt(parseInt(current, 36)), 0n)
-    .toString(36);
+    .toString();
 }
